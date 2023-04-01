508e357fa21d280d4da08ff10f6ff0ab1d59045b
Luiz Ramos <96121758+ramosluizfg@users.noreply.github.com>
2023-04-01 14:26:34 -0300

    Fix database name

--- main-db.tf
+++ main-db.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "5.7"
-  db_name              = "video-training"
+  db_name              = "videotraining"
   port                 = 3306
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
