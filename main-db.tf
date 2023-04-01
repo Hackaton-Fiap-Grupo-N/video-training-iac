71042a3c4d9e1bf02a0c982d6a4111d41a60bf3c
Luiz Ramos <96121758+ramosluizfg@users.noreply.github.com>
2023-04-01 16:11:55 -0300

    Add ECS

--- main-db.tf
+++ main-db.tf
@@ -8,13 +8,14 @@ data "aws_secretsmanager_secret_version" "db_password" {
   depends_on = [aws_secretsmanager_secret_version.db_password]
 }
 
-resource "aws_db_instance" "default" {
+
+resource "aws_db_instance" "production_db" {
   depends_on = [aws_secretsmanager_secret_version.db_password]
 
   identifier           = "production-db"
   instance_class       = "db.t2.micro"
   storage_type         = "gp2"
-  allocated_storage    = 20
+  allocated_storage    = 5
   engine               = "mysql"
   engine_version       = "5.7"
   db_name              = "videotraining"
@@ -24,4 +25,8 @@ resource "aws_db_instance" "default" {
 
   username = "dbadmin"
   password = data.aws_secretsmanager_secret_version.db_password.secret_string
+
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.id
+  vpc_security_group_ids = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
+  publicly_accessible    = true
 }
---
+++ main-ecs.tf
@@ -0,0 +1,66 @@
+
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_ecr_repository" "video_training_api" {
+  name = "video_training_api"
+}
+
+resource "aws_iam_role" "ecs_agent" {
+  name               = "ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  depends_on = [aws_iam_role.ecs_agent]
+  role       = "aws_iam_role.ecs_agent.name"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
+}
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+  image_id             = "ami-094d4d00fd7462815"
+  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+  security_groups      = [aws_security_group.ecs_sg.id]
+  user_data            = "#!/bin/bash\necho ECS_CLUSTER=production-cluster >> /etc/ecs/ecs.config"
+  instance_type        = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+  name                      = "asg"
+  launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+  desired_capacity          = 2
+  min_size                  = 2
+  max_size                  = 3
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "production-cluster"
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "api"
+  container_definitions = templatefile("${path.module}/api-task-definition.tftpl", { REPOSITORY_URL = aws_ecr_repository.video_training_api.repository_url })
+}
+
+resource "aws_ecs_service" "video_training_api" {
+  name            = "video-training-api"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
+}
